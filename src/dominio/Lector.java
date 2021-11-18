7a14dc9a9cfb9273c264641e3e99d470e8fe76d3
Luis <laclvl99@gmail.com>
2021-11-17 23:54:30 -0300

    Ultima entrega

--- src/dominio/Lector.java
+++ src/dominio/Lector.java
@@ -9,6 +9,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalTime;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lector {
 
@@ -16,19 +18,20 @@ public class Lector {
         List<Informacion> informaciones = new ArrayList<>();
         //Cargo la iformación del archivo.
         try {
-            String cadena;
-            FileReader archivo = new FileReader("src/resource/Caminos1.txt");
+            String cadena = "";
+            FileReader archivo = new FileReader("src/resource/Caminos.txt");
             BufferedReader b = new BufferedReader(archivo);
             //Cargo el archivo completo en el objeto m de la clase metodos, para separar los campos.
             b.readLine();
             while((cadena = b.readLine())!=null) {
                 String[] datos = cadena.split("\t");
-
+                if(!cadena.isEmpty()) {
                     String km = datos[3];
+                    String[] s = datos[0].trim().split("\\s+");
                     String kms = km.replaceAll(",", ".");
-                    Informacion c = new Informacion(datos[0], datos[1], Double.valueOf(kms), Integer.parseInt(datos[2]));
+                    Informacion c = new Informacion(s[0], datos[1], Double.valueOf(kms), Integer.parseInt(datos[2]));
                     informaciones.add(c);
-
+                }
             }
             b.close();
         } catch (FileNotFoundException e) {
@@ -43,20 +46,19 @@ public class Lector {
         List<Horario> horarios = new ArrayList<>();
         //Cargo la iformación del archivo.
         try {
-            String cadena;
-            FileReader archivo = new FileReader("src/resource/DatosClientes1.txt");
+            String cadena = "";
+            FileReader archivo = new FileReader("src/resource/DatosClientes.txt");
             BufferedReader b = new BufferedReader(archivo);
             //Cargo el archivo completo en el objeto m de la clase metodos, para separar los campos.
             b.readLine();
             while((cadena = b.readLine())!=null) {
                 String[] datos = cadena.split("\t");
-
-
+                if(!cadena.isEmpty()) {
                     if (!datos[0].contains("A"))
-                        horarios.add(new Horario(datos[0],LocalTime.of(Integer.valueOf(datos[4]),0),LocalTime.of(Integer.valueOf(datos[5]),0)));
+                        horarios.add(new Horario(datos[0], LocalTime.of(Integer.valueOf(datos[4]), 0), LocalTime.of(Integer.valueOf(datos[5]), 0)));
                     else
-                        horarios.add(new Horario(datos[0],LocalTime.of(0,0),LocalTime.of(0,0)));
-
+                        horarios.add(new Horario(datos[0], LocalTime.of(0, 0), LocalTime.of(0, 0)));
+                }
             }
             b.close();
         } catch (FileNotFoundException e) {
@@ -65,5 +67,12 @@ public class Lector {
             System.out.println("Problemas para leer el archivo.");
         }
         return horarios;
+    }
+
+    public static String getOnlyDigits(String s) {
+        Pattern pattern = Pattern.compile("[^0-9]");
+        Matcher matcher = pattern.matcher(s);
+        String number = matcher.replaceAll(";");
+        return number;
     }
 }
